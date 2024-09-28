3fb750bdb506a6f132c982622a4c84467c87a7ca
Erik Icket <37545039+eicket@users.noreply.github.com>
2024-09-28 12:51:22 +0200

    Delete scripts directory

--- scripts/KMSforSF.bat
+++
@@ -1,4 +0,0 @@
-D:
-cd D:\erik\java\KMSforSF
-"C:\Program Files\Java\jdk-17.0.1\bin\java" --module-path "D:\Erik\Java downloads\Java FX\openjfx-21.0.4_windows-x64_bin-sdk\javafx-sdk-21.0.4\lib" --add-modules javafx.controls,javafx.fxml -Djava.util.logging.config.file=file_logging.properties -jar dist/KMSforSF.jar
-rem exit
